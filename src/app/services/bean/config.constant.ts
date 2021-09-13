dbace15a53adcae38ecc83c0eceb59cd4b65b036
holyhost <2024212728@qq.com>
2021-09-13 22:59:40 +0800

    add a new branch: html

--- src/app/services/bean/config.constant.ts
+++
@@ -1,6 +0,0 @@
-export enum Config {
-    AppName = 'random_call',
-    Theme = 'theme-style',
-    Language = 'my-language',
-    RandomTime = 'random-time',
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  supportedLocale:['en-US','zh-CN']
-};
